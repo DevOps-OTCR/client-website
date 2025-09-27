15acdc963b679c10721ec1dde26cc9a1b1fae265
Laksh Sharma <lakshsharma@Lakshs-MacBook-Pro.local>
2025-09-27 10:29:53 -0500

    client website fix

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/otcr-consulting-nexus-web">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
